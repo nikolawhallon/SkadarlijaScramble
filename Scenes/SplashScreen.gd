0e4b1832d5e9d8de4c52efd9d1dcccc3a88fdb87
Nikola Whallon <nikola.whallon@gmail.com>
2024-03-14 15:05:52 -0700

    added joystick support

--- Scenes/SplashScreen.gd
+++ Scenes/SplashScreen.gd
@@ -2,6 +2,11 @@ extends Node2D
 
 var game_scene = preload("res://Scenes/Game.tscn").instantiate()
 
+# uncomment to special full-screen browser builds
+# I do this in order to get these Godot 4 games working on Raspberry Pi
+#func _input(event):
+#	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
 func _process(_delta):
 	if Input.is_action_just_pressed("start"):
 		#get_tree().root.add_child(game_scene) # doesn't delete this scene
